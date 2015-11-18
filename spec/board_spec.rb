30c582cf76d1eee38954863da5d97e38ff5af057
James Anthony Lowenthal <loweja01@gmail.com>
2015-11-18 09:32:07 -0500

    move #get_move test to computer_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,46 +1,84 @@
 require 'spec_helper'
 describe Board do
   let(:board) { Board.new }
-  describe "@spaces" do
-    it "has length of 9" do
-      expect(board.spaces.length).to eq 9
-    end
-  end
-  describe "@win_status" do
-    context "tie" do
-      it "contains tie message as string" do
+  context "attributes" do
+    describe "@spaces" do
+      it "has length of 9" do
+        expect(board.spaces.length).to eq 9
       end
     end
-    context "player X win" do
-      it "contains player X win message as string" do
-      end
-    end
-    context "player O win" do
-      it "contains player O win message as string" do
-      end
-    end
-  end
-  describe "#game_over?" do
-    context "game over" do
-      xit "outputs true" do
-      end
-    end
-    context "game not over" do
-      xit "outputs false" do
-      end
-    end
-  end
-  describe "#until_valid_move?" do
-    context "loops until valid input selected" do
-      it "outputs move, marker hash" do 
-        try_again = -> { puts "try again!" }
-        move = board.until_valid_move?(try_again) do
-          [0, "X"] 
+    describe "@win_status" do
+      context "tie" do
+        it "contains tie message as string" do
+          board = Board.new(spaces: ["O", "X", "O",
+                                     "X", "X", "O",
+                                     "O", "O", "X"])
+          board.game_over?
+          expect(board.win_status).to eq "Tie!"
         end
-        expect(move).to eq Hash[:move, 0, :marker, "X"]
       end
-      xit "calls try_again if not reached on first loop" do
+      context "player X win" do
+        it "contains player X win message as string" do
+          board = Board.new(spaces: ["X", "X", "O",
+                                     "X", "X", "O",
+                                     "O", "O", "X"])
+          board.game_over?
+          expect(board.win_status).to eq "Player X wins!"
+        end
+      end
+      context "player O win" do
+        it "contains player O win message as string" do
+          board = Board.new(spaces: ["O", "X", "O",
+                                     "O", "X", "O",
+                                     "O", "O", "X"])
+          board.game_over?
+          expect(board.win_status).to eq "Player O wins!"
+        end
       end
     end
   end
-end
+  context "public methods" do
+    describe "#game_over?" do
+      context "game over" do
+        it "outputs true" do
+          board = Board.new(spaces: ["O", "X", "O",
+                                     "O", "X", "O",
+                                     "O", "O", "X"])
+          expect(board.game_over?).to be_truthy
+        end
+      end
+      context "game not over" do
+        it "outputs false" do
+          board = Board.new(spaces: ["O", "X", "O",
+                                     nil, "X", "O",
+                                     "O", "O", "X"])
+          expect(board.game_over?).to be_falsey
+        end
+      end
+    end
+    describe "#valid_move?" do
+      context "valid move" do
+         it "returns true" do
+           expect(board.valid_move? 4).to be_truthy 
+         end
+      end
+      context "invalid move" do
+        it "returns false if space taken" do
+          board.place Hash[:move, 4, :marker, "X"] 
+          expect(board.valid_move? 4).to be_falsey
+        end
+        it "returns false if space does not exist" do
+          expect(board.valid_move? 10).to be_falsey
+        end
+      end
+    end
+    describe "#place" do
+      context "accepts player_move hash" do
+        it "places move accordingly" do
+          board.place Hash[:move, 5, :marker, "O"]
+          expect(board.spaces[5]).to eq "O"
+        end
+      end
+    end
+  end
+ end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,5 +3,19 @@ describe Computer do
   describe "#choose_marker" do
   end
   describe "#get_move" do
+    context "given a large number of games" do
+      it "computer players will always tie" do
+        outcomes = Array.new(1000).map do 
+          board = Board.new 
+          ["X", "O"].cycle do |marker|
+            move = Computer.get_move(marker, board)
+            board.place Hash[:move, move, :marker, marker]
+            break if board.game_over?
+          end
+          board.win_status
+        end.uniq
+        expect(outcomes.all? {|win_st| win_st == "Tie!"}).to be_truthy
+      end
+    end
   end
 end
